a678d133db7a563cd120cff7044c8e79d17fc141
Menna Khaliel <56710050+Menna13@users.noreply.github.com>
2021-01-03 14:26:23 -0600

    final work, camera intent

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,12 +3,17 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
             .applicationId("gB5SGiB3JuXL3cb60SAOcPXNPAN0At1lC8FgWJWe")
             .clientKey("LI0RLx5rxRBVq2XjeVUWuNhEO9fSY5uq959ppdOa")
